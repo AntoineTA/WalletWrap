2a43e35fea230c47d210a27f70a0bcebf94d2207
AntoineTA <an.taillard@gmail.com>
2024-05-13 23:15:10 +0200

    basic headers

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next"
 import { Inter as FontSans } from "next/font/google"
 import { cn } from "@/lib/utils"
 import "./globals.css"
+import Header from "@/components/Header"
 
 const fontSans = FontSans({
   subsets: ["latin"],
@@ -26,7 +27,10 @@ export default function RootLayout({
           fontSans.variable
         )}
       >
-        {children}
+        <Header />
+        <main>
+          {children}
+        </main>
       </body>
     </html>
   );
