0de328b35aa97f79cd61d325b942123a03259360
AntoineTA <an.taillard@gmail.com>
2024-05-14 12:29:22 +0200

    started app layout

---
+++ app/budget/layout.tsx
@@ -0,0 +1,25 @@
+import Link from 'next/link';
+
+export default function BudgetLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return(
+    <div className="grid min-h-screen w-full md:grid-cols-[220px_1fr] lg:grid-cols-[280px_1fr]">
+      <div className="hidden border-r bg-muted/40 md:block">
+        <div className="flex h-full max-h-screen flex-col gap-2">
+          <div>Side Panel Header</div>
+          <nav>Desktop Nav</nav>
+        </div>
+      </div>
+      <div className="flex flex-col">
+        <main>
+          Main
+          {children}
+        </main>
+        <div className="fixed bottom-0 w-full h-12 border-t bg-muted/40 flex items-center space-around md:hidden">Nav mobile</div>
+      </div>
+    </div>
+  )
+}
