2a98381fd5fe6e6317632e1410476d6487ae4a32
AntoineTA <an.taillard@gmail.com>
2024-05-15 23:47:08 +0200

    protect main layout

--- app/(main)/layout.tsx
+++ app/(main)/layout.tsx
@@ -1,25 +1,35 @@
-import { SideNav, BottomNav } from "@/components/Navigation";
+import { redirect } from "next/navigation"
 
-export default function BudgetLayout({
+import { SideNav, BottomNav } from "@/components/Navigation"
+import { createClient } from "@/utils/supabase/server"
+
+export default async function MainLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  return(
-    <div className="grid min-h-screen w-full md:grid-cols-[220px_1fr] lg:grid-cols-[280px_1fr]">
-      <div className="hidden border-r bg-muted/40 md:block">
-        <div className="flex h-full max-h-screen flex-col gap-2">
-          <div>Side Panel Header</div>
-          <SideNav />
+  const supabase = createClient()
+  const { data: { user } } = await supabase.auth.getUser()
+
+  if (!user) {
+    redirect("/login")
+  } else {
+    return (
+      <div className="grid min-h-screen w-full md:grid-cols-[220px_1fr] lg:grid-cols-[280px_1fr]">
+        <div className="hidden border-r bg-muted/40 md:block">
+          <div className="flex h-full max-h-screen flex-col gap-2">
+            <div>Side Panel Header</div>
+            <SideNav />
+          </div>
+        </div>
+        <div className="flex flex-col">
+          <main>
+            Main
+            {children}
+          </main>
+          <BottomNav />
         </div>
       </div>
-      <div className="flex flex-col">
-        <main>
-          Main
-          {children}
-        </main>
-        <BottomNav />
-      </div>
-    </div>
-  )
+    )
+  }
 }
